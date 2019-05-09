a23aa63baa6a0be7dbea8b7edf66ad041a939dfe
Al Wold <a@trusona.com>
2019-05-09 16:28:25 -0700

    Add doc comment

--- lib/trusona/paired_tru_code.rb
+++ lib/trusona/paired_tru_code.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 module Trusona
+  ##
+  # A TruCode that has already been paired by a device.
   class PairedTruCode
     def self.find(trucode_id)
       Trusona::Workers::PairedTruCodeFinder.new.find(trucode_id)
